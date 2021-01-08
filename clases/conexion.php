d09b771a77ce4bfe4d4fc77f7a58b1383616e008
lupeshll <66766177+lupeshll@users.noreply.github.com>
2021-01-08 13:04:40 -0500

    cambio de algunos datos como el puerto:3360

---
+++ clases/conexion.php
@@ -0,0 +1,24 @@
+<?php
+    class conectar{
+        private $servidor="localhost";
+        private $usuario="root";
+        private $password="";
+        private $bd="ventas";
+        private $puerto="3360";
+
+        public function conexion(){
+            $conexion=mysqli_connect($this->servidor,
+                                    $this->usuario,
+                                    $this->password,
+                                    $this->bd,
+                                    $this->puerto);
+            return $conexion;
+        }
+    }
+
+    /* $obj= new conectar();
+    if($obj->conexion()){
+        echo "conectado";
+    } */
+
+?>
