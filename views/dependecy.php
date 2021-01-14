d2e196daf36f410f1002383f1e91cefefb952f22
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-13 20:50:30 -0500

    enlasar con  el css/menu.css
    liena 9

--- views/dependecy.php
+++ views/dependecy.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/themes/default.css">
 <link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="../librerias/select2/css/select2.css">
+<link rel="stylesheet" type="text/css" href="../css/menu.css">
 
 <script src="../librerias/jquery-3.2.1.min.js"></script>
 <script src="../librerias/alertifyjs/alertify.js"></script>
