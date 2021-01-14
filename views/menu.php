91e8824f9a2197a8ef2ccb89da57dc7451fd7f1d
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-13 20:51:00 -0500

    Update menu.php

--- views/menu.php
+++ views/menu.php
@@ -10,6 +10,54 @@
     ?>
 </head>
 <body>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- Begin Navbar -->
+<div id="nav">
+  <div class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="100">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img class="img-responsive logo" src="https://cdn4.iconfinder.com/data/icons/adore/118/Camera.png" alt=""></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Home</a>
+            </li>
+            <li><a href="#about">About</a>
+            </li>
+            <li><a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+      <!--/.contatiner -->
+</div>
+</div>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
     
+    <!-- /container -->     
 </body>
 </html>
+<script type="text/javascript">
+    $(window).scroll(function() {
+		if ($(document).scrollTop() > 150) {
+            alert('hi');
+			$('.logo').height(200);
+
+		}
+		else {
+            $('.logo').height(100);
+        }
+	});
+</script>
