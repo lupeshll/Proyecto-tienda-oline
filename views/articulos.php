b7db205fe1367549e70b798903022b092d4d567a
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-16 23:46:42 -0500

    Maquetacion de los articulos
    Se hizo la maquetacion del subinidice articulos que tendra la tienda en el menu

--- views/articulos.php
+++ views/articulos.php
@@ -15,9 +15,63 @@
     <?php require_once "menu.php"; ?>
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Articulos</h1>
+        <div class="row">
+            <div class="col-sm-4">
+                <form id="frmArticulos" enctype="multipart/form-data">
+                    <label>Categoria</label>
+                    <select class="form-control input.sm" id="categoriaSelect" name="categoriaSelect">
+                        <option value="A">Selecciona Categoria</option>
+                    </select>
+                    <label>Nombre</label>
+                    <input type="text" class="form-control input-sm" id="nombre" name="nombre">
+                    <label>Descripcion</label>
+                    <input type="text" class="form-control input-sm" id="descripcion" name="descripcion">
+                    <label>Cantidad</label>
+                    <input type="text" class="form-control input-sm" id="cantidad" name="cantidad">
+                    <label>Precio</label>
+                    <input type="text" class="form-control input-sm" id="precio" name="precio">
+                    <label>Imagen</label>
+                    <input type="file" id="imagen" name="imagen">
+                    <p></p>
+                    <span id="btnAgregarArticulo" class="btn btn-primary">Agregar</span>    
+                </form>
+            </div>
+            <div class="col-sm-8">
+            <div id="tablaArticulosLoad"></div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#tablaArticulosLoad').load("articulos/tablaArticulos.php");
+        $('#btnAgregarArticulo').click(function(){
+            vacios=validarFormVacio('frmArticulos');
+
+            if(vacios > 0){
+                alertify.alert("Debes completar todos los campos");
+				return false;
+			}
+
+            datos=$('#frmArticulos').serialize();
+            $.ajax({
+                type:"POST",
+                data:datos,
+                url:"../procesos/articulos/agregaArticulos.php",
+                success:function(r){
+                    if(r==1){
+                        alertife.success("Agregad con exito");
+                    }else{ 
+                        alertife.error("No se pudo agregar ");
+                    }
+                }
+            });
+        });
+    });
+</script>
 <?php
     }else{
         header("location: /proyectoJL/login.php");
