7e19623cd19f51ab8d11cad7367c5c23e63691ab
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-17 00:31:36 -0500

    Maquetacion de los usuarios
    Se hizo la maquetacion a los usuarios de la tienda

--- views/usuarios.php
+++ views/usuarios.php
@@ -15,9 +15,63 @@
     <?php require_once "menu.php"; ?>
 </head>
 <body>
-    
+    <div class="container">
+    <h1>Administrar Usuarios</h1>
+    <div class="row">
+        <div class="col-sm-4">
+        <form id="frmRegistro">
+                    <label>Nombre</label>
+                    <input type="text" class="form-control input-sm" id="nombre" name="nombre">
+                    <label>Apellido</label>
+                    <input type="text" class="form-control input-sm" id="apellido" name="apellido">
+                    <label>Usuario</label>
+                    <input type="text" class="form-control input-sm" id="usuario" name="usuario">
+                    <label>Password</label>
+                    <input type="text" class="form-control input-sm" id="password" name="password">
+                    <p></p>
+                    <span  class="btn btn-primary" id="registro">Registrar</span>
+                            
+                        </form>
+            </div>
+            <div class="col-sm-7">
+            <div id="tablaUsuariosLoad"></div>
+            </div>
+        </div>          
+    </div>
 </body>
 </html>
+<script type="text/javascript">
+	$(document).ready(function(){
+
+        $('#tablaUsuariosLoad').load('usuarios/tablaUsuarios.php');
+		$('#registro').click(function(){
+
+			vacios=validarFormVacio('frmRegistro');
+
+			if(vacios > 0){
+                alertify.alert("Debes completar todos los campos");
+				return false;
+			}
+
+			datos=$('#frmRegistro').serialize();
+			$.ajax({
+				type:'POST',
+				data:datos,
+				url:"../procesos/regLogin/registrarUsuario.php",
+				success:function(r){
+                   
+					if(r==1){
+                        $('#tablaUsuariosLoad').load('usuarios/tablaUsuarios.php');
+                        alertify.success("Registro exitoso");
+                    }else{
+                        alertify.error("Fallo al agregar");
+                    }
+
+				}
+			});
+		});
+	});
+</script>
 <?php
     }else{
         header("location: /proyectoJL/login.php");
