3150d67568c305157b374058ac8de306f5fedbc9
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-16 23:48:03 -0500

    Agregacion de un script
    Se agrego un script   haciendo referencia a las funciones.js

--- views/dependecy.php
+++ views/dependecy.php
@@ -12,3 +12,4 @@
 <script src="../librerias/alertifyjs/alertify.js"></script>
 <script src="../librerias/bootstrap/js/bootstrap.js"></script>
 <script src="../librerias/select2/js/select2.js"></script>
+<script src="../js/funciones.js"></script>
