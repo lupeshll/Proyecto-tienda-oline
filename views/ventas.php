84761f5903fc6ea5d7697e6d2ecff84e390462f4
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-21 12:49:04 -0500

    Maquetacion de las ventas
    Se hizo la maquetacion de las ventas en el menu de la pagina

--- views/ventas.php
+++ views/ventas.php
@@ -15,9 +15,41 @@
     <?php require_once "menu.php"; ?>
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Venta de Productos</h1>
+        <div class="row">
+            <div class="col-sm-12">
+                <span class="btn btn-default" id="ventaProductosBtn">Vender Producto</span>
+                <span class="btn btn-default" id="ventasHechasBtn">Ventas hechas</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div id="ventaProductos"></div>
+                <div id="ventasHechas"></div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#ventaProductosBtn').click(function(){
+            esconderSeccionVenta();
+            $('#ventaProductos').load('ventas/ventasDeProductos.php');
+            $('#ventaProductos').show();
+        });
+        $('#ventasHechasBtn').click(function(){
+            esconderSeccionVenta();
+            $('#ventasHechas').load('ventas/ventasyReportes.php');
+            $('#ventasHechas').show();         
+        });
+    });
+    function esconderSeccionVenta(){
+        $('#ventaProductos').hide();
+        $('#ventasHechas').hide();
+    }
+</script>
 <?php
     }else{
         header("location: /proyectoJL/login.php");
