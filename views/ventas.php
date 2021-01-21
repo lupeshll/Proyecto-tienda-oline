cf91485ba2c45d68a3f68a8ff4edf8923e627a7d
lupeshll <66766177+lupeshll@users.noreply.github.com>
2021-01-21 16:40:42 -0500

    Agregando la referencia del SELECT2
    Se debía corregir la referencia del plugin jQuery: SELECT2.

--- views/ventas.php
+++ views/ventas.php
@@ -12,7 +12,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas</title>
-    <?php require_once "menu.php"; ?>
+    <?php require_once "menu.php"; 
+    ?>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
 </head>
 <body>
     <div class="container">
