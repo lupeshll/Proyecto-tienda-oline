95cb1c0402f9c8a374823eaafe145aef521c402f
yoelobregon <75326928+yoelobregon@users.noreply.github.com>
2021-01-22 16:18:04 -0500

    Agregando metodos post para insertar articulo

--- procesos/articulos/insertaArticulos.php
+++ procesos/articulos/insertaArticulos.php
@@ -1,15 +1,39 @@
 <?php 
+    session_start();
+    $iduser=$_SESSION['iduser'];
+    require_once "../../clases/conexion.php";
+    require_once "../../clases/articulos.php";
+
+    $obj= new articulos();
     
-
+    $datos=array();
+    
     $nombreImg=$_FILES['imagen']['name'];
+    $rutaAlmacenamiento=$_FILES['imagen']['tmp_name'];
+    $carpeta='../../archivos/';
+    $rutaFinal=$carpeta.$nombreImg;
 
-    $datos=array(
+    $datosImg=array(
         $_POST['categoriaSelect'],
-        $_POST['nombre'],
-        $_POST['descripcion'],
-        $_POST['cantidad'],
-        $_POST['precio'],
-        $nombreImg
-            );
-    print_r($datos);
+        $nombreImg,
+        $rutaFinal
+        );
+            if(move_uploaded_file($rutaAlmacenamiento, $rutaFinal)){
+                 $idimagen=$obj->agregaImagen($datosImg);
+                if($imagen > 0){
+                    $datos[0]=$_POST['categoriaSelect'];
+                    $datos[1]=$idimagen;
+                    $datos[2]=$iduser;
+                    $datos[3]=$_POST['nombre'];
+                    $datos[4]=$_POST['descripcion'];
+                    $datos[5]=$_POST['cantidad'];
+                    $datos[6]=$_POST['precio'];
+
+                    echo $obj->insertaArticulo($datos);
+
+                }else{
+                    echo 0;
+                }
+
+            }
 ?>
