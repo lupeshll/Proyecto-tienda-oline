8ccaba42f14d7ab7acafb4338ee1b615322453ee
lupeshll <66766177+lupeshll@users.noreply.github.com>
2021-01-22 01:00:41 -0500

    Creacion del archivo y sentencias SQL para la actualización de categorias
    Se creo el archivo php para que reciba los datos de del id y nombre a actualizar de la categoria. Además de crear las sentencias UPDATE para la actualización de registros.
    También se agrego un modal (ventana emergente) que permita realizar el update en aquel modal.

---
+++ procesos/categorias/actualizaCategoria.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once "../../clases/conexion.php";
+    require_once "../../clases/categorias.php";
+
+    
+
+    $datos=array(
+        $_POST['idcategoria'],
+        $_POST['categoriaU']
+    );
+
+    $obj=new categorias();
+
+    echo $obj->actualizarCategoria($datos)
+?>
