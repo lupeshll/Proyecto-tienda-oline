a64bcdd691d334d188fb7c1f8ca6b9e17ba6323a
lupeshll <66766177+lupeshll@users.noreply.github.com>
2021-01-29 07:39:57 -0500

    Actaulizacion de librerias, estilos, e index

--- views/ventas/reporteVentaPdf.php
+++ views/ventas/reporteVentaPdf.php
@@ -1,7 +1,94 @@
 <?php
     require_once "../../clases/conexion.php";
+    require_once "../../clases/ventas.php";
+
+
+    $objv= new ventas();
     $c= new conectar();
     $conexion=$c->conexion();
 
-    echo $idventa=$_GET['idventa'];
+    $idventa=$_GET['idventa'];
+
+    $sql="SELECT ve.id_venta,
+                ve.fechaCompra,
+                ve.id_cliente,
+                art.nombre,
+                art.precio,
+                art.descripcion
+            from ventas as ve
+            inner join  producto as art
+            on ve.id_producto=art.id_producto
+            and ve.id_venta='$idventa'";
+
+    $result=mysqli_query($conexion,$sql);
+
+        $ver=mysqli_fetch_row($result);
+
+        $folio=$ver[0];
+        $fecha=$ver[1];
+        $idcliente=$ver[2];
+       
 ?>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+            <title>Reporte Venta</title>
+            <link rel="stylesheet" type="text/css" href="../../librerias/bootstrap/css/bootstrap.css">
+    </head>
+    <body>
+        <img src="../../img/logo.png" width="200" height="200">
+        <br>
+        <table class="table">
+            <tr>
+                <td>Fecha:<?php echo $fecha; ?></td>
+            </tr>
+            <tr>
+                <td>Folio:<?php echo $folio ?></td>
+            </tr>
+            <tr>
+                <td>cliente:<?php echo $objv->nombreCliente($idcliente); ?></td>
+            </tr>
+        </table>
+     
+        <table class="table">
+            <tr>
+                <td>Nombre producto</td>
+                <td>Precio</td>
+                <td>Cantidad</td>
+                <td>Descripcion</td>
+            </tr>
+
+                <?php 
+                $sql="SELECT ve.id_venta,
+                            ve.fechaCompra,
+                            ve.id_cliente,
+                            art.nombre,
+                            art.precio,
+                            art.descripcion
+                        from ventas as ve
+                        inner join  producto as art
+                        on ve.id_producto=art.id_producto
+                        and ve.id_venta='$idventa'";
+
+                $result=mysqli_query($conexion,$sql);
+                $total=0;
+                while($mostrar=mysqli_fetch_row($result)):
+                ?>
+
+            <tr>
+                <td><?php echo $ver[3]; ?></td>
+                <td><?php echo $ver[4]; ?></td>
+                <td>1</td>
+                <td><?php echo $ver[5]; ?></td>
+            </tr>
+            <?php  
+                $total=$total + $ver[4];
+            endwhile;
+            ?>
+            <tr>
+                <td>Total= <?php echo "$".$total; ?></td>
+            </tr>
+        </table>
+    </body>
+    </html>
