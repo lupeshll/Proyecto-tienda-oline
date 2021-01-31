6b53c4b01f03e617bd727d445ed5527037d7118c
lupeshll <66766177+lupeshll@users.noreply.github.com>
2021-01-31 12:33:07 -0500

    Agregando y corrigiendo los estilos de nuestro index y login

--- login.php
+++ login.php
@@ -17,23 +17,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TrendStoore| Login de usuario</title>
     <link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
+    
     <script src="librerias/jquery-3.2.1.min.js"></script>
     <script src="js/funciones.js"></script>
     
 </head>
-<body style="background-color: gray">
+<body style="background-color: black">
+    
     <br><br><br>
     <div class="container">
         <div class="row">
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
-                <div class="panel panel-primary">
-                    <div class="panel panel-heading">
+                <div class="panel" style="border: 2px solid #ffffff;">
+                    <div class="panel panel-heading" style="background-color: black;font-size: 15px; color: #ffffff;">
                         Trend Stoore -
                     </div>
                     <div class="panel panel-body">
                         <p>
-                            <center><img src="img/logo.png" width="150px" height="150px"></center>
+                            <center><img src="img/logo1.png" width="150px" height="150px"></center>
                         </p>
                         <form id="frmLogin">
                             <label>Usuario</label>
